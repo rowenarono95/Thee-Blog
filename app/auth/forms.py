9837021dae04d0d378806160c59084ed0451005e
rowenarono95 <rowenarono@gmail.com>
2020-11-01 12:04:02 +0300

    add bootstrap link

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo,ValidationError
 from ..models import User
-from wtforms import ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
@@ -12,8 +13,8 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
-            if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('There is an account with that email')
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
--- app/models.py
+++ app/models.py
@@ -1,30 +1,39 @@
-from datetime import datetime
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin, current_user
+from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class Quote:
-    '''
-    Quote class to define quote Objects
-    '''
-
-    def __init__(self,id,author,quote):
-        self.id =id
-        self.author = author
-        self.quote = quote       
-
-class User(UserMixin, db.Model):
+  
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique = True, index = True)
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
-    blog = db.relationship('Blog',backref = 'user',lazy = "dynamic")  
+    blog = db.relationship('Blog', backref='username', lazy='dynamic')
+    comments = db.relationship('Comment', backref='username', lazy=True)
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.username}'
