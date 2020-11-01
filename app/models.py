27ade3a95f443c8ab5c887d13de080b5b5badd76
rowenarono95 <rowenarono@gmail.com>
2020-11-02 00:04:34 +0300

    add profile pic

--- app/models.py
+++ app/models.py
@@ -37,3 +37,49 @@ class User(UserMixin,db.Model):
     
     def __repr__(self):
         return f'User {self.username}'
+
+
+
+class Blog(db.Model):
+    __tablename__ = 'blog'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    title_blog = db.Column(db.String(255), index=True)
+    description = db.Column(db.String(255), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='CASCADE'), nullable=False)
+   
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_blog(cls, id):
+        blog = Blog.query.filter_by(id=id).all()
+        return blog
+    @classmethod
+    def get_all_blog(cls):
+        blog = Blog.query.order_by('-id').all()
+        return blog
+    def __repr__(self):
+        return f'Blog {self.blog_title}'        
+
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text())
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id',ondelete='CASCADE'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='CASCADE'))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_comments(cls, blog_id):
+        comments = Comment.query.filter_by(blog_id=blog_id).all()
+        return comments
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'Comments: {self.comment}'      
